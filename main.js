1d302728514246554e14c5f688623ee5180b8c2e
Momo Ramadori <momoramadori8@gmail.com>
2020-05-04 14:40:17 +0200

    Fatto!

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
 //intercetto il click sulla freccia "avanti" e sul suo box
-$('.next').click(function(){
+$('.next').click(nextF);
+
+//intercetto il click sulla freccia "indietro" e sul suo box
+$('.prev').click(prevF);
+
+//intercetto il click sui pallini
+$('.bullets-on i').click(function() {
+    //recupero la posizione del pallino attualmente cliccato
+    var currentIndex = $(this).index();
+    //collego l'immagine corrente legandomi alla posizione del pallino cliccato
+    var currentElement = $('img').eq(currentIndex);
+    //sottraggo la visibilità all'immagine che la aveva
+    $('img.active').removeClass('active');
+    //aggiungo la visibilità all'imagine corrispondente al pallino cliccato
+    currentElement.addClass('active');
+    //sottraggo la visibilità al pallino cliccato
+    $('i.actived').removeClass('actived');
+    //attribuisco la visibilità al pallino su cui mi sto spostando
+    $(this).addClass('actived');
+});
+
+
+//Per riuscire a rendere automatico lo scorrere delle slide a 3 secondi ed avviarlo/stopparlo con un click sui pulsanti play e pause riutilizzo la funzione precedentemente utilizzata per il next.
+
+//creo una variabile fuori dalla funzione così da renderla globale
+var clock;
+//intercetto il click su play e gli applico il setInterval per far scorrere le immagini
+$('.play').click(function() {
+    clock = setInterval(nextF, 3000);
+});
+//Intercetto il click su pause per interrompere il loop del setInterval tramite la funzione designata
+$('.pause').click(function() {
+    clearInterval(clock);
+});
+
+
+//funzione generalizzata next
+function nextF() {
     //recupero l'immagine corrente
     var currentImg = $('img.active');
     //recupero il pallino corrente
@@ -21,10 +58,10 @@ $('.next').click(function(){
         $('img:first-child').addClass('active');
         $('.fas:first-child').addClass('actived');
     }
-})
+};
 
-//intercetto il click sulla freccia "indietro" e sul suo box
-$('.prev').click(function(){
+//funzione generalizzata prev
+function prevF () {
     //recupero l'immagine corrente
     var currentImg = $('img.active');
     //recupero il pallino corrente
@@ -46,21 +83,4 @@ $('.prev').click(function(){
         $('img:last-child').addClass('active');
         $('.fas:last-child').addClass('actived');
     }
-});
-
-//intercetto il click sui pallini
-$('.bullets-on i').click(function() {
-    //recupero la posizione del pallino attualmente cliccato
-    var currentIndex = $(this).index();
-    console.log($(this).index());
-    //collego l'immagine corrente legandomi alla posizione del pallino cliccato
-    var currentElement = $('img').eq(currentIndex);
-    //sottraggo la visibilità all'immagine che la aveva
-    $('img.active').removeClass('active');
-    //aggiungo la visibilità all'imagine corrispondente al pallino cliccato
-    currentElement.addClass('active');
-    //sottraggo la visibilità al pallino cliccato
-    $('i.actived').removeClass('actived');
-    //attribuisco la visibilità al pallino su cui mi sto spostando
-    $(this).addClass('actived');
-});
+}
